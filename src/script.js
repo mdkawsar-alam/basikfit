2d2fedd689a94a3d5dfaddbcd753f621195d3e69
kawsar <mdkawsaralam537@gamil.com>
2023-09-26 10:54:31 +0600

    header section add on home page

---
+++ src/script.js
@@ -0,0 +1,7 @@
+
+
+let mobile_icon=document.getElementById("main-button");
+let main_menu=document.getElementById("main-menu");
+   mobile_icon.addEventListener('click',()=>{
+    main_menu.classList.toggle('hidden');
+});
